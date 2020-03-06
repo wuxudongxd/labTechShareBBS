10dbccd9a139bdfa72621bef52614f04de89052c
ActivePeter <1020401660@qq.com>
2020-03-06 21:42:38 +0800

    导航栏更新

--- src/components/content/navmenu/NavMenu.vue
+++ src/components/content/navmenu/NavMenu.vue
@@ -2,11 +2,31 @@
   <div class="header">
     <div class="content">
       <div class="logo left">
-        <router-link to="/"><img src="~assets/img/易控logo.png" alt="易控logo" v-if="this.setcont()"></router-link>
+        <router-link to="/"><img src="~assets/img/易控logo.png" alt="易控logo" v-if="this.islab()"></router-link>
       </div>
-      <ul class="nav left">
-        <li :key="key" v-for="(nav,key) in nav_list">
-          <router-link :to="nav.link">{{nav.name}}</router-link>
+      <ul class="nav left" v-if="islab()">
+        <li> 
+          <router-link to="/lab">首页</router-link>
+        </li>
+        <li> 
+          <router-link to="/lab">易控简介</router-link>
+        </li>
+        <li> 
+          <router-link to="/lab">培养方案</router-link>
+        </li>
+        <li> 
+          <router-link to="/lab">活动记录</router-link>
+        </li>
+        <li> 
+          <router-link to="/lab">项目记录</router-link>
+        </li>
+      </ul>
+      <ul class="nav left" v-if="!islab()">
+        <li> 
+          <router-link to="/bbs">首页</router-link>
+        </li>
+        <li> 
+          <router-link to="/bbsIndex">检索</router-link>
         </li>
       </ul>
 
@@ -32,9 +52,11 @@
           </el-menu>
         </div>
       </div>
-
+      
       <div v-if="!token" class="login-bar right">
-        <div class="login-box login-box2 left">
+        <el-button round @click="toBBS()" v-if="islab()">前往社区</el-button>
+        <div class="login-box login-box2">
+          
           <router-link to="/login">登录</router-link>&nbsp;|&nbsp;
           <router-link to="/register">注册</router-link>
         </div>
@@ -49,17 +71,19 @@ export default {
   data() {
     return {
       token: "",
-      nav_list: [
-        { id: "1", name: "首页", link: "/lab" },
-        { id: "2", name: "易控成员", link: "/users" },
-        { id: "3", name: "易控课堂", link: "/class" }
-      ]
+      navlist:[
+          { id: "1", name: "首页", link: "/lab" },
+          { id: "2", name: "易控成员", link: "/users" },
+          { id: "3", name: "易控课堂", link: "/class" }
+        ]
+        ,
     };
   },
   methods: {
-
-    setcont(){
-      console.log("adsadd:"+this.$route.path);
+    toBBS(){
+      this.$router.push({path: '/bbs'})
+    },
+    islab(){
       return this.$route.path=="/lab"?true:false;
     },
     handleSelect(key, keyPath) {
@@ -116,6 +140,6 @@ export default {
 }
 
 .login-bar {
-  margin: 20px 0 0 0;
+  margin: 10px 0 0 0;
 }
 </style>
